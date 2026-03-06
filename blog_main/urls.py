f24e2b01d08b4233d82dd4a8c6307412fb1d7589
Hassaan Hussain <hafizhassaan114@gmail.com>
2026-03-06 14:47:09 +0500

    User Authentication

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.urls import path, re_path
 from . import views
 from blogs import views as blogViews
 
@@ -27,7 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/', blogViews.blog, name='blog'),
-    path('blogs/search/', blogViews.search, name='search')
+    path('blog/<slug:slug>/', blogViews.blog, name='blog'),
+    path('blogs/search/', blogViews.search, name='search'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 
-] + [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
